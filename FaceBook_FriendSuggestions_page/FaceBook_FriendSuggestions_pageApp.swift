c5b3fc3afd7cb9cfffb9371bbbe6dbbe839767ef
Syed Raza <syed.raza011@gmail.com>
2023-06-23 21:44:00 -0400

    Initial Commit

---
+++ FaceBook_FriendSuggestions_page/FaceBook_FriendSuggestions_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceBook_FriendSuggestions_pageApp.swift
+//  FaceBook_FriendSuggestions_page
+//
+//  Created by Syed Raza on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceBook_FriendSuggestions_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
